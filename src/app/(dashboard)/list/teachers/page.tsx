3dcaf082d501d55c231419c2b680ab2b55f05957
wali55 <walisantunu@gmail.com>
2025-03-15 15:22:35 +0600

    pagination

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -3,20 +3,13 @@ import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
 import { role, teachersData } from "@/lib/data";
+import { prisma } from "@/lib/prisma";
+import { ITEM_PER_PAGE } from "@/lib/settings";
+import { Class, Subject, Teacher } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
 
-type Teacher = {
-  id: number;
-  teacherId: string;
-  name: string;
-  email?: string;
-  photo: string;
-  phone: string;
-  subjects: string[];
-  classes: string[];
-  address: string;
-};
+type TeacherType = Teacher & { subjects: Subject[] } & { classes: Class[] };
 
 const columns = [
   { header: "Info", accessor: "info" },
@@ -36,41 +29,61 @@ const columns = [
   { header: "Actions", accessor: "action", className: "table-cell" },
 ];
 
-const TeacherListPage = () => {
-  const renderRow = (item: Teacher) => (
-    <tr key={item.id} className="border-b border-gray-200 text-sm even:bg-slate-50 hover:bg-waliPurpleLight">
-      <td className="flex items-center gap-4 p-4">
-        <Image
-          src={item.photo}
-          alt=""
-          width={40}
-          height={40}
-          className="md:hidden xl:block size-10 rounded-full object-cover"
-        />
-        <div className="flex flex-col">
-          <h3 className="font-semibold">{item.name}</h3>
-          <p className="text-xs text-gray-500">{item?.email}</p>
-        </div>
-      </td>
-      <td className="hidden md:table-cell">{item.teacherId}</td>
-      <td className="hidden md:table-cell">{item.subjects.join(",")}</td>
-      <td className="hidden md:table-cell">{item.classes.join(",")}</td>
-      <td className="hidden lg:table-cell">{item.phone}</td>
-      <td className="hidden lg:table-cell">{item.address}</td>
-      <td>
-        <div className="flex items-center gap-2">
-          <Link href={`/list/teachers/${item.id}`}>
-            <button className="size-7 flex items-center justify-center rounded-full bg-waliSky">
-              <Image src="/view.png" alt="" width={16} height={16} />
-            </button>
-          </Link>
-          {role === "admin" && (
-            <FormModal table="teacher" type="delete" id={item.id} />
-          )}
-        </div>
-      </td>
-    </tr>
-  );
+const renderRow = (item: TeacherType) => (
+  <tr
+    key={item.id}
+    className="border-b border-gray-200 text-sm even:bg-slate-50 hover:bg-waliPurpleLight"
+  >
+    <td className="flex items-center gap-4 p-4">
+      <Image
+        src={item.img || "/noAvatar.png"}
+        alt=""
+        width={40}
+        height={40}
+        className="md:hidden xl:block size-10 rounded-full object-cover"
+      />
+      <div className="flex flex-col">
+        <h3 className="font-semibold">{item.name}</h3>
+        <p className="text-xs text-gray-500">{item?.email}</p>
+      </div>
+    </td>
+    <td className="hidden md:table-cell">{item.username}</td>
+    <td className="hidden md:table-cell">{item.subjects.map((subject) => subject.name).join(",")}</td>
+    <td className="hidden md:table-cell">{item.classes.map((classItem) => classItem.name).join(",")}</td>
+    <td className="hidden lg:table-cell">{item.phone}</td>
+    <td className="hidden lg:table-cell">{item.address}</td>
+    <td>
+      <div className="flex items-center gap-2">
+        <Link href={`/list/teachers/${item.id}`}>
+          <button className="size-7 flex items-center justify-center rounded-full bg-waliSky">
+            <Image src="/view.png" alt="" width={16} height={16} />
+          </button>
+        </Link>
+        {role === "admin" && (
+          <FormModal table="teacher" type="delete" id={item.id} />
+        )}
+      </div>
+    </td>
+  </tr>
+);
+
+const TeacherListPage = async ({searchParams}: {
+  searchParams: {[key:string]: string | undefined}
+}) => {
+  const {page, ...queryParams} = searchParams;
+  const p = page ? parseInt(page) : 1;
+
+  const [data, count] = await prisma.$transaction([
+    prisma.teacher.findMany({
+      include: {
+        subjects: true,
+        classes: true
+      },
+      take: ITEM_PER_PAGE,
+      skip: ITEM_PER_PAGE * (p - 1)
+    }),
+    prisma.teacher.count()
+  ])
   
   return (
     <div className="bg-white rounded-md p-4 flex-1 m-4 mt-0">
@@ -91,10 +104,10 @@ const TeacherListPage = () => {
         </div>
       </div>
       {/* List */}
-      <Table columns={columns} renderRow={renderRow} data={teachersData} />
+      <Table columns={columns} renderRow={renderRow} data={data} />
       {/* Pagination */}
-      
-      <Pagination />
+
+      <Pagination page={p} count={count} />
     </div>
   );
 };
--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,17 +1,41 @@
-const Pagination = () => {
-    return (
-        <div className="p-4 flex items-center justify-between text-gray-500">
-            <button disabled className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed">Prev</button>
-            <div className="flex items-center gap-2 text-sm">
-                <button className="px-2 rounded-sm bg-waliSky">1</button>
-                <button className="px-2 rounded-sm">2</button>
-                <button className="px-2 rounded-sm">3</button>
-                ...
-                <button className="px-2 rounded-sm">10</button>
-            </div>
-            <button className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed">Next</button>
-        </div>
-    )
-}
-export default Pagination;
+"use client"
 
+import { ITEM_PER_PAGE } from "@/lib/settings";
+import { useRouter } from "next/navigation";
+
+const Pagination = ({ page, count }: { page: number; count: number }) => {
+  const router = useRouter();
+
+  const hasPrev = ITEM_PER_PAGE * (page - 1) > 0;
+  const hasNext = ITEM_PER_PAGE * (page - 1) + ITEM_PER_PAGE < count;
+
+  const changePage = (newPage: number) => {
+    const params = new URLSearchParams(window.location.search);
+    params.set("page", newPage.toString());
+    router.push(`${window.location.pathname}?${params}`);
+  }
+  return (
+    <div className="p-4 flex items-center justify-between text-gray-500">
+      <button
+        onClick={() => changePage(page - 1)}
+        disabled={!hasPrev}
+        className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed"
+      >
+        Prev
+      </button>
+      <div className="flex items-center gap-2 text-sm">
+        {Array.from(
+          { length: Math.ceil(count / ITEM_PER_PAGE) },
+          (_, index) => {
+            const pageIndex = index + 1;
+            return <button onClick={() => changePage(pageIndex)} key={pageIndex} className={`px-2 rounded-sm ${page === pageIndex ? "bg-waliSky": ""}`}>{pageIndex}</button>;
+          }
+        )}
+      </div>
+      <button disabled={!hasNext} onClick={() => changePage(page + 1)} className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed">
+        Next
+      </button>
+    </div>
+  );
+};
+export default Pagination;
