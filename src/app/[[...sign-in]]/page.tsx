8db54622532f0fa659d582b07b22f86452e5f68c
wali55 <walisantunu@gmail.com>
2025-03-22 21:31:45 +0600

    protected routes

--- src/app/[[...sign-in]]/page.tsx
+++ src/app/[[...sign-in]]/page.tsx
@@ -2,9 +2,22 @@
 
 import * as Clerk from "@clerk/elements/common";
 import * as SignIn from "@clerk/elements/sign-in";
+import { useUser } from "@clerk/nextjs";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const LoginPage = () => {
+    const { isSignedIn, user, isLoaded } = useUser();
+    const router = useRouter();
+
+    useEffect(() => {
+      const role = user?.publicMetadata.role;
+      if (role) {
+        router.push(`/${role}`);
+      }
+    }, [user, router]);
+
   return (
     <div className="h-screen flex items-center justify-center bg-waliSkyLight">
       <SignIn.Root>
@@ -16,19 +29,38 @@ const LoginPage = () => {
             <Image src="/logo.png" alt="" width={24} height={24} />
             <h1 className="text-xl font-bold">School Management App</h1>
           </div>
-          <h2 className="text-gray-400 text-center mb-2">Signin to your account</h2>
+          <h2 className="text-gray-400 text-center mb-2">
+            Signin to your account
+          </h2>
           <Clerk.GlobalError className="text-sm text-red-400" />
           <Clerk.Field name="identifier" className="flex flex-col gap-2">
-            <Clerk.Label className="text-xs text-gray-500">Username</Clerk.Label>
-            <Clerk.Input type="text" required className="p-2 rounded-md border border-gray-300" />
+            <Clerk.Label className="text-xs text-gray-500">
+              Username
+            </Clerk.Label>
+            <Clerk.Input
+              type="text"
+              required
+              className="p-2 rounded-md border border-gray-300"
+            />
             <Clerk.FieldError className="text-xs text-red-400" />
           </Clerk.Field>
           <Clerk.Field name="password" className="flex flex-col gap-2">
-            <Clerk.Label className="text-xs text-gray-500">Password</Clerk.Label>
-            <Clerk.Input type="password" required className="p-2 rounded-md border border-gray-300" />
+            <Clerk.Label className="text-xs text-gray-500">
+              Password
+            </Clerk.Label>
+            <Clerk.Input
+              type="password"
+              required
+              className="p-2 rounded-md border border-gray-300"
+            />
             <Clerk.FieldError className="text-xs text-red-400" />
           </Clerk.Field>
-          <SignIn.Action submit className="bg-blue-500 text-white rounded-md p-[10px] my-1 hover:bg-blue-600">Sign In</SignIn.Action>
+          <SignIn.Action
+            submit
+            className="bg-blue-500 text-white rounded-md p-[10px] my-1 hover:bg-blue-600"
+          >
+            Sign In
+          </SignIn.Action>
         </SignIn.Step>
       </SignIn.Root>
     </div>
