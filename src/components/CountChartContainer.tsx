27503c43fbcb730b294f81170007fea0cd7d80dc
wali55 <walisantunu@gmail.com>
2025-04-04 14:57:54 +0600

    count chart complete

--- src/components/CountChartContainer.tsx
+++ src/components/CountChartContainer.tsx
@@ -9,6 +9,8 @@ const CountChartContainer = async () => {
     })
 
     const boys = data.find((item) => item.sex === "MALE")?._count || 0;
+    const girls = data.find((item) => item.sex === "FEMALE")?._count || 0;
+
     return (
         <div className="bg-white rounded-xl w-full h-full p-4">
               {/* TITLE */}
@@ -16,18 +18,18 @@ const CountChartContainer = async () => {
                 <h1 className="text-lg font-semibold">Students</h1>
                 <Image src="/moreDark.png" alt="" width={20} height={20} />
               </div>
-              <CountChart />
+              <CountChart boys={boys} girls={girls} />
               {/* BOTTOM */}
               <div className="flex justify-center gap-16">
                 <div className="flex flex-col gap-1">
                   <div className="w-5 h-5 bg-waliSky rounded-full" />
-                  <h1 className="font-bold">1,234</h1>
-                  <h2 className="text-xs text-gray-300">Boys (50%)</h2>
+                  <h1 className="font-bold">{boys}</h1>
+                  <h2 className="text-xs text-gray-300">Boys ({Math.round(boys / (boys + girls) * 100)}%)</h2>
                 </div>
                 <div className="flex flex-col gap-1">
                   <div className="w-5 h-5 bg-waliYellow rounded-full" />
-                  <h1 className="font-bold">1,234</h1>
-                  <h2 className="text-xs text-gray-300">Girls (50%)</h2>
+                  <h1 className="font-bold">{girls}</h1>
+                  <h2 className="text-xs text-gray-300">Girls ({Math.round(girls / (boys + girls) * 100)}%)</h2>
                 </div>
               </div>
             </div>
