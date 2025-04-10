19c1a172d281851ba354bbfbf51b90bbc718230f
wali55 <walisantunu@gmail.com>
2025-04-10 20:06:01 +0600

    adjust current week schedule

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,50 @@
 const currentWorkWeek = () => {
-    const today = new Date();
-    const dayOfWeek = today.getDay();
+  const today = new Date();
+  const dayOfWeek = today.getDay();
 
-    const startOfWeek = new Date(today);
+  const startOfWeek = new Date(today);
 
-    if (dayOfWeek === 0) {
-        startOfWeek.setDate(today.getDate() + 1);
-    }
+  if (dayOfWeek === 0) {
+    startOfWeek.setDate(today.getDate() + 1);
+  }
+
+  if (dayOfWeek === 6) {
+    startOfWeek.setDate(today.getDate() + 2);
+  } else {
+    startOfWeek.setDate(today.getDate() - (dayOfWeek - 1));
+  }
+
+  startOfWeek.setHours(0, 0, 0, 0);
+
+  const endOfWeek = new Date(startOfWeek);
+  endOfWeek.setDate(endOfWeek.getDate() + 4);
+
+  endOfWeek.setHours(23, 59, 59, 999);
+
+  return { startOfWeek, endOfWeek };
+};
+
+export const adjustScheduleToCurrentWeek = (lessons: {title:string;start:Date;end: Date}[]):{title:string;start:Date;end: Date}[] => {
+    const {startOfWeek, endOfWeek} = currentWorkWeek();
+
+    return lessons.map((lesson) => {
+        const lessonDayOfWeek = lesson.start.getDay();
+
+        const daysFromMonday = lessonDayOfWeek === 0 ? 6 : lessonDayOfWeek - 1;
+
+        const adjustedStartDate = new Date(startOfWeek);
+        adjustedStartDate.setDate(startOfWeek.getDate() + daysFromMonday);
+        const adjustedEndDate = new Date(adjustedStartDate);
+        adjustedEndDate.setHours(
+            lesson.end.getHours(),
+            lesson.end.getMinutes(),
+            lesson.end.getSeconds()
+        )
+
+        return {
+            title: lesson.title,
+            start: adjustedStartDate,
+            end: adjustedEndDate,
+        }
+    })
 }
